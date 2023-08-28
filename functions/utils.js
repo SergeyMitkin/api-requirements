7a0e73f82246e0ac324b4e2f9214f63a6198cab7
SergeyMitkin <sergeymitkin@gmail.com>
2023-08-28 14:14:33 +0300

    request data and time is taken from request

--- functions/utils.js
+++ functions/utils.js
@@ -1,35 +1,8 @@
 const crypto = require("crypto");
 
-function getDateStr(){
-    let date = new Date();
-    return date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear();
-}
-
-function sortObject(unordered, sortArrays = false) {
-    if (!unordered || typeof unordered !== 'object') {
-        return unordered;
-    }
-
-    if (Array.isArray(unordered)) {
-        const newArr = unordered.map((item) => sortObject(item, sortArrays));
-        if (sortArrays) {
-            newArr.sort();
-        }
-        return newArr;
-    }
-
-    const ordered = {};
-    Object.keys(unordered)
-        .sort()
-        .forEach((key) => {
-            ordered[key] = sortObject(unordered[key], sortArrays);
-        });
-    return ordered;
-}
-
-function sha256(time, sorted_user_params, salt){
+function sha256(time, req_data, salt){
     let hash = crypto.createHash('sha256');
-    hash.update(time + sorted_user_params + salt);
+    hash.update(time + req_data + salt);
 
     return hash;
 }
@@ -47,8 +20,6 @@ function isAlphabetSorted(data) {
 }
 
 module.exports = {
-    getDateStr: getDateStr,
     sha256: sha256,
-    sortObject: sortObject,
     isAlphabetSorted: isAlphabetSorted
 }
